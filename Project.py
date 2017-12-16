244e60ce24ceede23e209417ce20eb0eae2a2b97
JCxYIS <jsn811@gmail.com>
2017-12-16 17:25:29 +0800

    still...ShootAngle

--- Project.py
+++ Project.py
@@ -11,15 +11,17 @@ RocketHeight = 70       #火箭長度(m)
 RocketMass = 50.0    #火箭淨重 (kg)
 
 ShootAngle = 50.0
-Fuel1Mass = 550.0      #燃料1重量 (kg)
-Stage1SepHeight = 500 * 1000  #火箭1脫節高度(m)
-Fuel2Mass = 250.0      #燃料2重量 (kg)
+Fuel1Mass = 817.0      #燃料1重量 (kg)
+Stage1SepHeight = 8140.9 * 1000  #火箭1脫節高度(m)
+Fuel2Mass = 100.0      #燃料2重量 (kg)
 
 Stage = 1 #階段
 
 CurrentMass = RocketMass + Fuel1Mass + Fuel2Mass #後面有函式改這個值
-FuelEmitPS = 0.024  #燃料每秒排放質量 (kg)
-FuelEmitSpd = 5000.0   #燃料噴射速度(m/s)
+Fuel1EmitPS = 0.075  #燃料1每秒排放質量 (kg)
+Fuel1EmitSpd = 4876.3   #燃料1噴射速度(m/s)
+Fuel2EmitPS = 0.014  #燃料2每秒排放質量 (kg)
+Fuel2EmitSpd = 5000.0   #燃料2噴射速度(m/s)
 
 InitV = vector(0, 0, 0) #火箭初始速度
 
@@ -28,11 +30,12 @@ scene_Range = 870000 #攝影機拍攝範圍
 RocketTrailStyle = "points" #"points" or "trail" or "PoiCurve"
 SetPointFreq = 1000 #cmd紀錄/軌跡 時間間隔
 
-EarthRadius = 6371000.0 ; #地球半徑 (m)
+EarthRadius = 6371 * 1000.0 ; #地球半徑 (m)
 EarthMass = 5.97237 * (10**24) #地球質量(kg)
 
 dt = 0.01     #時間間隔
 t = 0.0
+RATE = 6666 #迴圈執行速度
 #INIT_SCENE#####################################################################
 scene = display(width=600, height=600, background=(0,0,0),
                 center=(0,1000,0),range=scene_Range)#設定畫面
@@ -41,7 +44,7 @@ scene.title = "Launch! With our Hopes and Dreams!"
 y_t = gdisplay(x=600,y=0,width=300,height=300,     #畫y-t圖
               title='x-t', xtitle='t', ytitle='Altitude (m)',
               foreground=color.black,background=color.white,
-              xmax=Graph_Tmax, xmin=0, ymax=Stage1SepHeight * 15, ymin=0)
+              xmax=Graph_Tmax, xmin=0, ymax=Stage1SepHeight * 5, ymin=0)
 f1 = gcurve(color=color.red)
 
 v_t = gdisplay(x=900,y=0,width=300,height=300,   #畫v-t圖
@@ -80,7 +83,7 @@ else:
 
 
 
-Yee = text(text='Yee', align='center', height= 7, depth=-0.93, color=color.green, pos = vector(0,4,0)) #Yee.
+Yee = text(text='Yee', align='center', height= 99999, depth=48763, color=color.green, pos = vector(0,200,0)) #Yee.
 
 rocket.pos = vector(0, 0, 0)        #初始位置
 rocket.axis = vector(0, RocketHeight, 0)
@@ -126,19 +129,33 @@ def print_UI():
         print Dialogue
 
 
+def KeyInput(Event):  # keyboard interrupt callback function,
+    global RATE, dt     # define the global variables that you want to change by this function
+    RATEmod = {'-': -66, '=': 66}
+    dtmod = {'[': 1.0/1.1, ']': 1.1}
+
+    s = Event.key
+    if s in RATEmod :
+        RATE += RATEmod[s]
+        print "Modified RATE to ", RATE
+    if s in dtmod:
+        dt *= dtmod[s]
+        print "Modified dt to ", dt
+scene.bind('keydown', KeyInput)                    # the binding method
+
 
 #LOOP 1######################################################################
 while Stage == 1: #phase 1
-    rate(6666)   #每一秒跑  次
+    rate(RATE)   #每一秒跑  次
     t = t + dt    #timer
 
     #燃料1噴射########################################################
     # M*Pre_ball.v.y = (M-dm)*ball.v.y+dm*(ball.v.y-u)  動量守恆
     if Fuel1Mass > 0: #燃料噴射
-        delta_v = FuelEmitPS * FuelEmitSpd / CurrentMass #速度增加量, according to the above formula(F=ma not working)
-        Fuel1Mass -= FuelEmitPS
+        delta_v = Fuel1EmitPS * Fuel1EmitSpd / CurrentMass #速度增加量, according to the above formula(F=ma not working)
+        Fuel1Mass -= Fuel1EmitPS
     else:
-        FuelMass = 0
+        Fuel1Mass = 0
         delta_v = 0
     rocket.a = vector(0, delta_v/dt, 0)
 
@@ -146,9 +163,6 @@ while Stage == 1: #phase 1
         rocket.a += g
     #if a < 0 : a = 0
 
-    #如果墜地##########################################################
-    if rocket.pos.y <= 0: #墜地
-        rocket.pos.y = 0; rocket.v.y = 0; #a = 0;
 
     #如果高度達第二次噴射#####################################################
     if rocket.pos.y > Stage1SepHeight:
@@ -170,6 +184,14 @@ while Stage == 1: #phase 1
     AntiRV = 1 / RV
     rocket.axis = rocket.v * AntiRV * RocketHeight
 
+
+    #如果墜地##########################################################
+    if(dist < EarthRadius and RV >= 1):
+        print "Ahhhhh!!! Rocket Crushed!!"
+        Stage = 66666
+    elif dist < EarthRadius:
+        rocket.y = 0
+
     #畫圖#############################################################
     Draw_pic()
 
@@ -179,16 +201,16 @@ while Stage == 1: #phase 1
 
 #LOOP 2######################################################################
 while Stage == 2: #phase 1
-    rate(6666)   #每一秒跑  次
+    rate(RATE)   #每一秒跑  次
     t = t + dt    #timer
 
     #燃料2噴射########################################################
     # M*Pre_ball.v.y = (M-dm)*ball.v.y+dm*(ball.v.y-u)  動量守恆
     if Fuel2Mass > 0: #燃料噴射
-        delta_v = FuelEmitPS * FuelEmitSpd / CurrentMass #速度增加量, according to the above formula(F=ma not working)
-        Fuel2Mass -= FuelEmitPS
+        delta_v = Fuel2EmitPS * Fuel2EmitSpd / CurrentMass #速度增加量, according to the above formula(F=ma not working)
+        Fuel2Mass -= Fuel2EmitPS
     else:
-        FuelMass = 0
+        Fuel2Mass = 0
         delta_v = 0
     rocket.a = vector(delta_v/dt , 0, 0)
 
@@ -214,7 +236,7 @@ while Stage == 2: #phase 1
     #如果墜地##########################################################
     if(dist < EarthRadius):
         print "Ahhhhh!!! Rocket Crushed!!"
-        break
+        Stage = 66666
 
     #畫圖############################################################
     Draw_pic()
@@ -223,5 +245,5 @@ while Stage == 2: #phase 1
     print_UI()
 
 
-while true:
+while Stage == 66666:
     rate(1)
